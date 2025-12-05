f0217860bdaa44380afde33a9dba8e12509d4ecd
typicaltaco116 <jwpyburn05@gmail.com>
2025-12-05 17:26:07 -0600

    Change subroutine_copy to copy by word. Limit scope.

--- src/SRAM_subroutine_copy.c
+++ src/SRAM_subroutine_copy.c
@@ -1,6 +1,9 @@
 #include "SRAM_subroutine_copy.h"
 
-void copy_subroutine(char* src_start, char* src_end, char* dst_start)
+#include <stdlib.h>
+#include <stdint.h>
+
+static void copy_subroutine(uint16_t* src_start, uint16_t* src_end, uint16_t* dst_start)
 // src_start is the start address of the subroutine to copy
 // src_end is the start address of the following function
 // dst_start is the pointer to the destination
@@ -20,11 +23,12 @@ void* malloc_subroutine(void* src_start, void* src_end)
    Space should be deallocated with free() when no longer needed.
 */
 {
-  void* tempPtr = malloc(src_end - src_start);
+  void* tempPtr = malloc((size_t)((uint32_t)src_end - (uint32_t)src_start));
+
   if (!tempPtr)
     return tempPtr; // not enough space in heap
 
-  copy_subroutine((char*)src_start, (char*)src_end, tempPtr);
+  copy_subroutine((uint16_t*)src_start, (uint16_t*)src_end, tempPtr);
 
-  return (void*)tempPtr;
+  return tempPtr;
 }
--- src/SRAM_subroutine_copy.h
+++ src/SRAM_subroutine_copy.h
@@ -1,7 +1,4 @@
 #pragma once
 #include <msp430.h>
-#include <stdlib.h>
-
-void copy_subroutine(char* src_start, char* src_end, char* dst_start);
 
 void* malloc_subroutine(void* src_start, void* src_end);
