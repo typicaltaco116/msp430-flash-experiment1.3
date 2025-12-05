953f3b32e4b9ecee4202d607b3d0f1344823bb62
typicaltaco116 <jwpyburn05@gmail.com>
2025-12-05 10:41:47 -0600

    Optimize bit checking function

--- src/flash_statistics.c
+++ src/flash_statistics.c
@@ -14,10 +14,9 @@ void fs_checkBits(f_segment_t seg, fs_stats_s* stats, uint16_t expected_val)
 // majority based voting
 // position of bits not set correctly
 {
-  uint16_t word_bin;
   uint16_t differences;
   uint16_t* read_head = (uint16_t*)seg;
-  uint8_t voted_bit;
+  uint8_t voted_difference_bit;
 
   stats->incorrect_bit_count = 0;
   stats->unstable_bit_count = 0;
@@ -25,38 +24,30 @@ void fs_checkBits(f_segment_t seg, fs_stats_s* stats, uint16_t expected_val)
   // Iterate through entire segment
   while(read_head < (uint16_t*)(seg + 1)){
 
-    uint16_t bit_votes[16] = {0};
     uint16_t difference_votes[16] = {0};
-
 
     // Perform repeated word reads
     for (uint8_t i = 0; i < STAT_READ_COUNT; i++){
 
-      word_bin = *read_head; // read
-      differences = expected_val ^ word_bin; // comparison
+      differences = expected_val ^ *read_head; // comparison
 
       // Gather votes
       for (uint8_t b = 0; b < 16; b++){
-        if (word_bin & (1 << b)){
-            bit_votes[b]++;
-        }
-
         if (differences & (1 << b)){
           difference_votes[b]++;
         }
       }
-
     }
 
     // Process vote arrays
     for (uint8_t b = 0; b < 16; b++) {
 
-      // Form voted word based on majority (6 or more)
-      voted_bit =  (bit_votes[b] >= (STAT_READ_COUNT / 2 + 1));
-      if (voted_bit ^ ((expected_val >> b) & 1)) // ISSUE IN ORIGINAL CODE
+      // Form voted bit based on majority (6 or more)
+      voted_difference_bit =  (difference_votes[b] >= (STAT_READ_COUNT / 2 + 1));
+      if (voted_difference_bit)
         stats->incorrect_bit_count++;
 
-      // Increment unstable bits if changed atleast once
+      // Increment unstable bits if wrong atleast once
       if (difference_votes[b] > 0)
         stats->unstable_bit_count++;
     }
